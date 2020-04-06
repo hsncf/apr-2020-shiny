46db53228e31887e236bd11da7d4dbbc87ddfaa3
Andriy V. Koval <koval.andrey@gmail.com>
2020-04-06 18:55:48 -0400

    reference table for project type
    #7 , store it in /.data-public/metadata/project_type.csv

---
+++ manipulation/todo/create-table-project-types.R
@@ -0,0 +1,17 @@
+project_types <- c(
+  "Emergency Shelter"
+  ,"Transitional Housing"
+  ,"PH - Permanent Supportive Housing"
+  ,"Street Outreach"
+  ,"RETIRED"
+  ,"Services Only"
+  ,"Other"
+  ,"Safe Haven"
+  ,"PH - Housing Only"
+  ,"PH - Housing with Services"
+  ,"Day Shelter"
+  ,"Homelessness Prevention"
+  ,"PH - Rapid Re-Housing"
+  ,"Coordinated Assessment"
+)
+names(project_types) <- 1:length(project_types)
