335b4edce0e41118cb3e1558e40a88733e5c2490
Kyle Belanger <kyleb44@hotmail.com>
2020-04-07 11:42:45 -0400

    add project type csv
    #7
    add project type csv

---
+++ analysis/general-purpose-scripts/project-type-creation.R
@@ -0,0 +1,21 @@
+projType <- c("Emergency Shelter"
+              ,"Transitional Housing"
+              ,"PH - Permanent Supportive Housing"
+              ,"Street Outreach"
+              ,"RETIRED"
+              ,"Services Only"
+              ,"Other"
+              ,"Safe Haven"
+              ,"PH - Housing Only"
+              ,"PH - Housing with Services"
+              ,"Day Shelter"
+              ,"Homelessness Prevention"
+              ,"PH - Rapid Re-Housing"
+              ,"Coordinated Assessment")
+
+
+projType1 <- tibble::tibble(1:14,projType)
+colnames(projType1) <- c("X","Project Type")
+
+
+readr::write_csv(projType1,"./data-public/metadata/project-type.csv")
