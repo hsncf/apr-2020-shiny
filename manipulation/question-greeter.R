0c4f300572e550a27161508bfadcf04048f53952
Andriy V. Koval <koval.andrey@gmail.com>
2020-04-20 17:12:53 -0400

    Update question-greeter.R

--- manipulation/question-greeter.R
+++ manipulation/question-greeter.R
@@ -56,8 +56,8 @@ dto <- allQuestions_new
 
 # ---- replace-quotes -------------
 
-replace_quotes <- function(ds){
-  ds %>% 
+replace_quotes <- function(d){
+  d_out <- d %>%
   dplyr::mutate_if(
     is.character,
     ~stringr::str_replace_all(
@@ -72,6 +72,7 @@ replace_quotes <- function(ds){
     is.character,
     ~trimws(.)
   )
+  return(d_out)
 }
 
 lsd <- dto
@@ -93,7 +94,7 @@ dto[["path"]][52] %>% names()
 dto[["path"]][57] %>% names()
 
 for(q_name in names(dto[["path"]]) ){
-  
+
   if(q_name != "q5a" ){
     # i <- 1
     dto[["data"]][[q_name]] <- read.csv(dto[["path"]][q_name] ,header = TRUE, stringsAsFactors = F) %>%
