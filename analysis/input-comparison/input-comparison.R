55416ef2e09f2eba6ea10721781ab507a28f6883
Andriy V. Koval <koval.andrey@gmail.com>
2020-05-05 18:24:08 -0400

    Update input-comparison.R

--- analysis/input-comparison/input-comparison.R
+++ analysis/input-comparison/input-comparison.R
@@ -213,36 +213,6 @@ a <- rep_new[["q23c"]] %>%
   dplyr::pull("Total")
 
 
-
-
-# ---- -------------------
-# if(is.null(input$aprZip))
-#   return(NULL)
-# projType <- as.numeric(allQuestions()[["q4a"]][4,2])
-# totalPersons <- allQuestions()[["q7a"]][5,2]
-# stayers <- allQuestions()[["q5a"]][8,1]
-# excluded <- allQuestions()[["q23a"]][41,2] + allQuestions()[["q23b"]][41,2]
-# goodDest <- allQuestions()[["q23a"]][13,2]+allQuestions()[["q23b"]][13,2]
-# posDestPer <- sprintf("%1.2f%%",100*goodDest/(allQuestions()[["q23a"]][39,2]+allQuestions()[["q23b"]][39,2]-excluded))
-# posDestStayPerPH <-sprintf("%1.2f%%",100*(goodDest+stayers)/(totalPersons-excluded))
-# if(projType==3)
-#   return(paste("Clients staying in PSH or exiting to permanent destinations:",posDestStayPerPH))
-# paste("Clients exiting to permanent destinations:",posDestPer)
-
-
-# projType <- as.numeric(allQuestions()[["q4a"]][4,2])
-# totalPersons <- allQuestions()[["q7a"]][5,2]
-# stayers <- allQuestions()[["q5a"]][8,1]
-# excluded <- allQuestions()[["q23a"]][41,2] + allQuestions()[["q23b"]][41,2]
-# goodDest <- allQuestions()[["q23a"]][13,2]+allQuestions()[["q23b"]][13,2]
-# posDestPer <- sprintf("%1.2f%%",100*goodDest/(allQuestions()[["q23a"]][39,2]+allQuestions()[["q23b"]][39,2]-excluded))
-# posDestStayPerPH <-sprintf("%1.2f%%",100*(goodDest+stayers)/(totalPersons-excluded))
-# if(projType==3)
-#    return(paste("Clients staying in PSH or exiting to permanent destinations:",posDestStayPerPH))
-# paste("Clients exiting to permanent destinations:",posDestPer)
-
-
-
 # ---- dqInc -------
 # incHousingMissing <- sum(allQuestions()[["q6c"]][,2])
 # HoH <- allQuestions()[["q5a"]][14,1] + allQuestions()[["q5a"]][15,1]
